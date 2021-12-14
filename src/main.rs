d588b216c7f281b3409466c37ed6a3c34d4f3dd0
Bryan Egbert <bryanegbert561@gmail.com>
2021-12-14 18:35:25 +0700

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(non_snake_case)]
-mod init;
+mod proxy;
 
-pub use crate::init::proxy::Proxy;
+use proxy::Proxy;
+
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -11,9 +12,32 @@ async fn main() -> Result<(), reqwest::Error> {
     
     // Proxy::create_proxy(&proxy).await?;
     let proxy_list = Proxy::get_proxy(&proxy).await?;
+
+    Proxy::new_har(&proxy).await?;
     
     Proxy::close(&proxy).await?;
 
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn create_proxy() -> Result<(), reqwest::Error> {
+        let proxy = Proxy {host: "localhost", port: 8081, path: "./browsermob-proxy-2.1.4/bin/browsermob-proxy"};
+
+        Proxy::create_proxy(&proxy).await?; 
+        let proxy_list = Proxy::get_proxy(&proxy).await?;
+
+        assert_eq!(proxy_list.contains("8"), true);
+
+        Proxy::close(&proxy).await?;
+        let proxy_list = Proxy::get_proxy(&proxy).await?;
+
+        assert_eq!(proxy_list, "null");
+
+        Ok(())
+    }
 }
