45d79ced0685c64dcfb66b45a1f6a6b63adf6ad6
Bryan Egbert <bryanegbert561@gmail.com>
2021-12-14 15:47:29 +0700

    Add browsermob-proxy helper function

--- src/init.rs
+++ src/init.rs
@@ -1,17 +1,55 @@
+
+
 pub mod proxy {
-	pub async fn initProxy() -> Result<(), reqwest::Error> {
-		let client = reqwest::Client::new();
-		client.post("http://localhost:8080/proxy").send().await?;
+	use std::process;
+
+	#[derive(Copy, Clone)]
+	pub struct Proxy {
+		pub host: &'static str,
+		pub port: u16,
+		pub path: &'static str
+	}
+	
+	impl Proxy {
+		pub fn run(&self){
+			let host = self.host;
+			let port = self.port.to_string();
+
+			if cfg!(target_os = "windows") {
+				process::Command::new(self.path).args(["--address", host, "--port", port.as_str()]).spawn().expect("failed to run proxy");
+			} else {
+				process::Command::new("sh").args([self.path, "--address", host, "--port", port.as_str()]).spawn().expect("failed to run proxy");
+			}
+
+		}
+
+		pub async fn create_proxy(&self) -> Result<(), reqwest::Error> {
+
+			let client = reqwest::Client::new();
+			client.post(format!("http://{}:{}/proxy", self.host, self.port)).send().await?;
 		
-		Ok(())
+			Ok(())
+		}
+
+		pub async fn get_proxy(&self) -> Result<String, reqwest::Error> {
+			let res = reqwest::get(format!("http://{}:{}/proxy", self.host, self.port)).await?
+				.text().await?;
+			let parsed_res = &json::parse(&res).unwrap()["proxyList"][0];
+			
+			Ok(parsed_res["port"].to_string())
+	
+		}
+		 
+		pub async fn close(&self) -> Result<(), reqwest::Error> {
+			let client = reqwest::Client::new();
+			let port = self.get_proxy().await?;
+
+			client.delete(format!("http://{}:{}/proxy/{}", self.host, self.port, port)).send().await?;
+			
+			Ok(())
+		}
+
+
 	}
 
-	pub async fn getProxy() -> Result<(), reqwest::Error> {
-		let res = reqwest::get("http://localhost:8080/proxy").await?;
-		let content = res.text().await?;
-
-		println!("{}", content);
-
-		Ok(())
-	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![allow(non_snake_case)]
 mod init;
-pub use crate::init::proxy;
+
+pub use crate::init::proxy::Proxy;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    println!("Hello, world!");
-    proxy::initProxy().await?;
-    proxy::getProxy().await?;
+    let proxy = Proxy {host: "localhost", port: 8081, path: "./browsermob-proxy-2.1.4/bin/browsermob-proxy"};
+
+    // Proxy::run(&proxy);
+    
+    // Proxy::create_proxy(&proxy).await?;
+    let proxy_list = Proxy::get_proxy(&proxy).await?;
+    
+    Proxy::close(&proxy).await?;
+
 
     Ok(())
 }
