2917d2e043bd6f91c77df9427b3f8c927ae1efce
Bryan Egbert <bryanegbert561@gmail.com>
2021-12-17 23:22:03 +0700

    Bug in generating har file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,56 @@
 #![allow(non_snake_case)]
 mod prelude;
 
-use thirtyfour::prelude::WebDriverResult;
+use thirtyfour::Capabilities;
+use thirtyfour::prelude::*;
 use prelude::*;
-
+use thirtyfour::common::capabilities::desiredcapabilities::Proxy; 
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
     let proxy = BrowserMobProxy {host: "localhost", port: 8081, path: "./browsermob-proxy-2.1.4/bin/browsermob-proxy"};
 
+    
     // BrowserMobProxy::run(&proxy);
     
-    BrowserMobProxy::create_proxy(&proxy).await?;
-    let proxy_list = BrowserMobProxy::get_proxy(&proxy).await?;
+    let res = proxy.create_proxy().await?;
+    println!("create proxy: {}", res);
+    let proxy_list = proxy.get_proxy().await?;
+    let proxy_port = 8082;
     println!("{}", proxy_list);
+    
+    let mut caps = DesiredCapabilities::edge();
+    // caps.set_proxy(Proxy::AutoConfig {url: format!("http://{}:{}", proxy.host, 8082)})?;
+    caps.set_proxy(Proxy::Manual {
+			ftp_proxy: None, 
+			http_proxy: Some(format!("http://{}:{}", proxy.host, proxy_port)), 
+			ssl_proxy: None,
+			socks_proxy: None,
+			socks_version: None,
+			socks_username: None,
+			socks_password: None,
+			no_proxy: None
+		})?;
+	caps.accept_ssl_certs(true)?;
 
-    let selenium = Selenium {url: "http://localhost:4445/wd/hub".to_string()};
+    println!("{}", caps.get_mut());
+    let selenium = Selenium::init(WebDriver::new("http://localhost:4445/wd/hub", &caps).await?);
 
-    selenium.run(&proxy).await?;
-    // BrowserMobProxy::new_har(&proxy).await?;
+    selenium.setup().await?;
+
+    
+    
+    BrowserMobProxy::new_har(&proxy).await?;
+
+    selenium.run().await?;
+    tokio::time::sleep(Duration::from_millis(2000)).await;
+
+    let har: String = BrowserMobProxy::get_har(&proxy).await?;
+
+    println!("{}", har);
+
+    selenium.quit().await?;
 
     Ok(())
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,7 @@
 use std::process;
 use thirtyfour::prelude::*;
-use thirtyfour::Capabilities;
-use thirtyfour::common::capabilities::desiredcapabilities::Proxy; 
+use std::time::Duration;
+use tokio;
 
 pub struct BrowserMobProxy {
 	pub host: &'static str,
@@ -10,7 +10,7 @@ pub struct BrowserMobProxy {
 }
 
 pub struct Selenium {
-	pub url: String
+	pub driver: WebDriver
 }
 
 impl BrowserMobProxy {
@@ -26,12 +26,14 @@ impl BrowserMobProxy {
 
 	}
 
-	pub async fn create_proxy(&self) -> Result<(), reqwest::Error> {
-
+	pub async fn create_proxy(&self) -> Result<reqwest::StatusCode, reqwest::Error> {
 		let client = reqwest::Client::new();
-		client.post(format!("http://{}:{}/proxy", self.host, self.port)).send().await?;
-	
-		Ok(())
+		let res = client.post(format!("http://{}:{}/proxy", self.host, self.port))
+			.query(&[("port", "8082"), ("bindAddress", self.host), ("serverBindAddress", self.host), ("trustAllServers", "true")])
+			.send()
+			.await?;
+
+		Ok(res.status())
 	}
 
 	pub async fn get_proxy(&self) -> Result<String, reqwest::Error> {
@@ -47,16 +49,18 @@ impl BrowserMobProxy {
 		let client = reqwest::Client::new();
 		let proxy = self.get_proxy().await?;
 
-		let res = client.put(format!("http://{}:{}/proxy/{}/har", self.host, self.port, proxy)).send().await?;
+		let res = client.put(format!("http://{}:{}/proxy/{}/har", self.host, self.port, proxy))
+			.query(&[("captureHeaders", "true"), ("initialPageTitle", "newbinusmaya")])
+			.send().await?;
 
 		Ok(res.status())
 	}
 
-	pub async fn get_har(&self) -> Result<reqwest::StatusCode, reqwest::Error> {
+	pub async fn get_har(&self) -> Result<String, reqwest::Error> {
 		let proxy = self.get_proxy().await?;
-		let res = reqwest::get(format!("http://{}:{}/proxy/{}/har", self.host, self.port, proxy)).await?;
+		let res = reqwest::get(format!("http://{}:{}/proxy/{}/har", self.host, self.port, proxy)).await?.text().await?;
 
-		Ok(res.status())
+		Ok(res.to_string())
 	}
 
 	pub async fn close(&self) -> Result<reqwest::StatusCode, reqwest::Error> {
@@ -70,17 +74,61 @@ impl BrowserMobProxy {
 }
 
 impl Selenium {
-	pub async fn run(&self, proxy: &BrowserMobProxy) -> WebDriverResult<()> {
-		let proxy_port = BrowserMobProxy::get_proxy(&proxy).await?;
+	async fn microsoft_login(&self) -> WebDriverResult<()> {
+		let mcr_login_btn = self.driver.find_element(By::ClassName("button--azure--ad")).await?; 
+		mcr_login_btn.click().await?;
 
-		let caps = DesiredCapabilities::edge().set_proxy(Proxy::AutoConfig {url: format!("http://localhost:{}", proxy_port)})?;
-		let driver = WebDriver::new(self.url.as_str(), &caps).await?;
+		let mcr_email = self.driver.find_element(By::Id("i0116")).await?;
+		tokio::time::sleep(Duration::from_millis(1000)).await;
+		
+		mcr_email.send_keys(TypingData::from("bryan.egbert@binus.ac.id") + Keys::Enter).await?;
+		tokio::time::sleep(Duration::from_millis(1000)).await;
 
-		driver.get("https://newbinusmaya.binus.ac.id").await?;
+		// self.driver.find_element(By::Id("idSIButton9")).await?.click().await?;
+		tokio::time::sleep(Duration::from_millis(1000)).await;
 
-		driver.quit().await?;
+		self.driver.find_element(By::Id("i0118")).await?.send_keys("ControlTheBoard90").await?;
+		self.driver.find_element(By::Id("idSIButton9")).await?.click().await?;
+
+		self.driver.find_element(By::Id("idSIButton9")).await?.click().await?;
+		
 
 		Ok(())
 	}
+
+	pub fn init(driver: WebDriver) -> Self {
+		Self {
+			driver
+		}
+	}
+
+	pub async fn setup(&self) -> WebDriverResult<()> {
+
+		
+
+		self.driver.set_implicit_wait_timeout(Duration::new(30, 0)).await?;
+
+		Ok(())
+
+	}
+	
+	pub async fn run(&self) -> WebDriverResult<()> {
+		// self.driver.get("https://www.google.com").await?;
+		self.driver.get("https://newbinusmaya.binus.ac.id").await?;
+	
+		Selenium::microsoft_login(&self).await?;
+	
+		tokio::time::sleep(Duration::from_millis(10000)).await;
+	
+		
+		Ok(())
+	}
+	
+	pub async fn quit(self) -> WebDriverResult<()> {
+		self.driver.quit().await?;
+
+		Ok(())
+	}
+	
 }
 
