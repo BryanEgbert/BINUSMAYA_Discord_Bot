cd5d3508aaf828d3170b0d3fcabaa9b03dd7239a
Bryan Egbert <bryanegbert561@gmail.com>
2022-02-25 16:38:51 +0700

    Bump up patch version

--- src/discord/commands/general/about.rs
+++ src/discord/commands/general/about.rs
@@ -12,7 +12,7 @@ async fn about(ctx: &Context, msg: &Message) -> CommandResult {
 		m.embed(|e| e
 			.colour(PRIMARY_COLOR)
 			.field("Made By:", "Bryan Egbert `PlayerPlay#9549`", false)
-			.field("Version:", "`v2.0.2`", true)
+			.field("Version:", "`v2.0.4`", true)
 			.field("Releases", "[Click here](https://github.com/BryanEgbert/BINUSMAYA_Discord_Bot/releases)", true)
 			.field("Bot General Info", "[Click here](https://github.com/BryanEgbert/BINUSMAYA_Discord_Bot)", false)
 			.footer(|f| f.text("This bot is open source. Any feedbacks or feature requests are welcomed"))
